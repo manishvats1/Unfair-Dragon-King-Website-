1f9d4bbd4eb3fdcb8c4c06f0b5c402db1ab7092a
manishvats1 <130773672+manishvats1@users.noreply.github.com>
2023-08-04 17:51:31 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmsUVmcSoZ6clJqDm3UteB9NW9B0Yepbo",
+    authDomain: "udk-website.firebaseapp.com",
+    projectId: "udk-website",
+    storageBucket: "udk-website.appspot.com",
+    messagingSenderId: "160962456690",
+    appId: "1:160962456690:web:5e2db53cac49b0c49266eb",
+    measurementId: "G-TNH8V7QV1N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
